573262aa0432083b19957eb0fd8fe6e4a0f4f36b
ecDusty <ecmgit@outlook.com>
2016-08-22 16:29:34 +0800

    FEAT: Added Promises to Call a list of different cat images
    FEAT: Added Promises to Call a list of different cat images
    
    Must remove where I call the images from. As gifs are extremely
    annoying. This was to test how to integrate an AJAX/ promise call to get
    an image from another source.

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,15 @@
 "use strict";
 //Bringing in the Fisher-Yates Array Shuffle Method. A great explanation and examples can be found here: https://bost.ocks.org/mike/shuffle/
 function shuffle(array) {
-  var m = array.length, t, i;
-  while (m) {
-    i = Math.floor(Math.random() * m--);
-    t = array[m];
-    array[m] = array[i];
-    array[i] = t;
-  }
+	var m = array.length,
+			t,
+			i;
+	while (m) {
+		i = Math.floor(Math.random() * m--);
+		t = array[m];
+		array[m] = array[i];
+		array[i] = t;
+	}
 }
 
 //How many images would you like to produce to choose from?
@@ -19,19 +21,36 @@ var numImgsAvailCounter = 1;
 
 //Array of Cat Names. I constantly add to this everytime I edit the JavaScript File
 var catNames = [
-  'Whiskers',
-  'Buttons',
-  'Mr. Wilkinson',
-  'James Bond',
-  'Ms. Patterson',
-  'Kronk',
-  'Titan'
+	'Whiskers',
+	'Buttons',
+	'Mr. Wilkinson',
+	'James Bond',
+	'Ms. Patterson',
+	'Kronk',
+	'Titan'
 ];
 var catNameCounter = 0;
 
 //Shuffle the Order of cat names. So that they  aren't always the same
 shuffle(catNames);
 
+var catImages = [];
+
+for (var i = 0; i < numImgs; i++) {
+	var count = 0;
+	catImages.push(fetch('http://thecatapi.com/api/images/get?format=src&type=gif')
+		.then(function(response) {
+			var a = i;
+			return (function (t) {
+					var catImg = document.getElementById('imgBox' + (count+1)).firstChild;
+					var catMenuItem = document.getElementById('menuitem'+(count+1));
+					console.log('Cat image url: ' + response.url + ', Cat counter: ' + (count+1) + ', ' + catImg);
+					catImg.src = response.url;
+					catMenuItem.src = response.url;
+					count++;
+			})(count);
+} ));
+}
 //Create a container to hold all the Cat Instances of the cats
 var iDisCon = document.createElement('div');
 iDisCon.id = 'image-container';
@@ -51,94 +70,95 @@ mDisCon.appendChild(mTitle);
 //Creates all the Cat Instances and puts them into the iDisCon
 for (var i = 0; i < numImgs; i++) {
 
-  //CREATE CAT INSTANCE
-  var v = i+1;
-  var clicks = 0;
-  var imgBox = document.createElement('div');
-  var img = document.createElement('img');
-  var label = document.createElement('h4');
-  var counter = document.createElement('h3');
-  var imgsrc = 'images/image-' + numImgsAvailCounter + '.jpg';
+	//CREATE CAT INSTANCE
+	var v = i+1;
+	var clicks = 0;
+	var imgBox = document.createElement('div');
+	var img = document.createElement('img');
+	var label = document.createElement('h4');
+	var counter = document.createElement('h3');
+	var imgsrc = 'images/image-' + numImgsAvailCounter + '.jpg';
 
-  //Add needed ID & Class to the Cat Instance container
-  imgBox.className = 'imgBox';
-  imgBox.id = 'imgBox' + v;
+	//Add needed ID & Class to the Cat Instance container
+	imgBox.className = 'imgBox';
+	imgBox.id = 'imgBox' + v;
 
-  //Sets up the Cat Image
-  img.className = 'clicker-image';
-  img.setAttribute('alt', ('Clicker Image ' + v + ', Hong Kong Web developer'));
-  img.setAttribute('src', imgsrc);
+	//Sets up the Cat Image
+	img.className = 'clicker-image';
+	img.setAttribute('alt', ('Clicker Image ' + v + ', Hong Kong Web developer'));
+	img.setAttribute('src', imgsrc);
 
-  //Creates a Name for the Cat Instance
-  label.className = 'image-label';
-  label.textContent = catNames[catNameCounter];
+	//Creates a Name for the Cat Instance
+	label.className = 'image-label';
+	label.textContent = catNames[catNameCounter];
 
-  //Creates the counter for the Cat Instance
-  counter.id = 'img' + v;
-  counter.className = 'imgCounter';
-  counter.innerHTML = 0;
+	//Creates the counter for the Cat Instance
+	counter.id = 'img' + v;
+	counter.className = 'imgCounter';
+	counter.innerHTML = 0;
 
-  //Puts the image, label, and counter into its specific Cat Instance Container
-  imgBox.appendChild(img);
-  imgBox.appendChild(label);
-  imgBox.appendChild(counter);
+	//Puts the image, label, and counter into its specific Cat Instance Container
+	imgBox.appendChild(img);
+	imgBox.appendChild(label);
+	imgBox.appendChild(counter);
 
-  //Mouse interaction. Everytime an image is clicked, the counter goes up
-  imgBox.addEventListener('click', (function (countID) {
-    return function () {
-      document.getElementById(countID).innerHTML ++;
-    }
-  })(counter.id));
+	//Mouse interaction. Everytime an image is clicked, the counter goes up
+	imgBox.addEventListener('click', (function (countID) {
+		return function () {
+			document.getElementById(countID).innerHTML ++;
+		}
+	})(counter.id));
 
-  //Keyboard interaction. Everytime a keyboard key is hit, the active image counter goes up
-  window.addEventListener('keydown', (function (countID) {
-    return function () {
-      if (document.getElementById(countID).parentElement.classList.contains('active'))
-        document.getElementById(countID).innerHTML ++;
-    }
-  })(counter.id));
+	//Keyboard interaction. Everytime a keyboard key is hit, the active image counter goes up
+	window.addEventListener('keydown', (function (countID) {
+		return function () {
+			if (document.getElementById(countID).parentElement.classList.contains('active'))
+				document.getElementById(countID).innerHTML ++;
+		}
+	})(counter.id));
 
-  //CREATE MENU ITEM
-  var menuItem = document.createElement('div');
-  var menuImg = document.createElement('img');
+	//CREATE MENU ITEM
+	var menuItem = document.createElement('div');
+	var menuImg = document.createElement('img');
+	menuImg.id = 'menuitem'+v;
 
-  //Setup img element for the menu button
-  menuImg.className = 'menu-item-image';
-  menuImg.setAttribute('alt', ('Menu Item Image ' + v + ', Hong Kong Web developer'));
-  menuImg.setAttribute('src', imgsrc);
+	//Setup img element for the menu button
+	menuImg.className = 'menu-item-image';
+	menuImg.setAttribute('alt', ('Menu Item Image ' + v + ', Hong Kong Web developer'));
+	menuImg.setAttribute('src', imgsrc);
 
-  menuItem.appendChild(menuImg);
+	menuItem.appendChild(menuImg);
 
 
 
-  //Clicking A Menu Item will show the corresponding Cat Instance
-  menuItem.addEventListener('click', (function (countID) {
-    return function () {
-      if (!document.getElementById(countID).parentElement.classList.contains('active')) {
-        //Hide the currently active Instance
-        document.getElementsByClassName('active')[0].classList.toggle('active');
-        //Show the selected Instance
-        document.getElementById(countID).parentElement.classList.toggle('active');
-      }
-    };
-  })(counter.id));
+	//Clicking A Menu Item will show the corresponding Cat Instance
+	menuItem.addEventListener('click', (function (countID) {
+		return function () {
+			if (!document.getElementById(countID).parentElement.classList.contains('active')) {
+				//Hide the currently active Instance
+				document.getElementsByClassName('active')[0].classList.toggle('active');
+				//Show the selected Instance
+				document.getElementById(countID).parentElement.classList.toggle('active');
+			}
+		};
+	})(counter.id));
 
-  //If there are more Cat instances than cat images available. Start from the beginning of the image counter.
-  if (numImgsAvailCounter < numImgsAvail) {
-    numImgsAvailCounter++;
-  } else {
-    numImgsAvailCounter = 1;
-  }
+	//If there are more Cat instances than cat images available. Start from the beginning of the image counter.
+	if (numImgsAvailCounter < numImgsAvail) {
+		numImgsAvailCounter++;
+	} else {
+		numImgsAvailCounter = 1;
+	}
 
-  //If there are more Cat instances than cat Names available. Reshuffle the Cat Names array and start from the beginning of the list
-  if (catNameCounter < catNames.length-1) {
-    catNameCounter++;
-  } else {
-    catNameCounter = 0;
-    shuffle(catNames);
-  }
-  mDisCon.appendChild(menuItem);
-  iDisCon.appendChild(imgBox);
+	//If there are more Cat instances than cat Names available. Reshuffle the Cat Names array and start from the beginning of the list
+	if (catNameCounter < catNames.length-1) {
+		catNameCounter++;
+	} else {
+		catNameCounter = 0;
+		shuffle(catNames);
+	}
+	mDisCon.appendChild(menuItem);
+	iDisCon.appendChild(imgBox);
 }
 
 iDisCon.firstChild.classList.toggle('active');
@@ -148,12 +168,34 @@ document.getElementById('display-container').appendChild(iDisCon);
 
 
 
-
-
-
-
-
-
+//var View = {
+//	init = function () {
+//
+//	},
+//	render = function () {
+//
+//	}
+//};
+//
+//var Model = {
+//	init = function () {
+//
+//	},
+//	render = function () {
+//
+//	}
+//};
+//
+//var Octopus = {
+//	init = function () {
+//
+//	},
+//	render = function () {
+//
+//	}
+//};
+//
+//
 
 
 
